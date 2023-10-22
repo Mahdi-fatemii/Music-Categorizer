96ee1f83be4d2b2f5b6bc9f6bf7d081e096ab131
Mahdi-fatemii <fatemikiamahdi67@gmail.com>
2023-10-22 12:11:54 +0330

    Update Music Categorizer.py

--- Music Categorizer.py
+++ Music Categorizer.py
@@ -3,5 +3,4 @@ from Music_Categorizer_funcs import *
 folder_path = "C:\\Users\\MT1ShotYT\\Desktop\\music"
    
 music_categorizer(folder_path, db_file_name='hard-rock.xlsx', folder_name='hard-rock')
-
-            
+         
